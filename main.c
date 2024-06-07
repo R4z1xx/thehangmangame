f142345af64bad627d7250cdc9c51492719b6a9c
R4z1xx <118757955+R4z1xx@users.noreply.github.com>
2024-06-07 08:41:31 +0200

    Add arbo

---
+++ main.c
@@ -0,0 +1,15 @@
+// Demande si mot aléatoire ou mot choisi
+
+// Affichage du message + les barres du mot à trouver
+
+// Demande de saisir d'une lettre + vérification de la lettre
+
+// Si lettre trouvée, affichage de la lettre dans le mot
+// Si lettre non trouvée, affichage du pendu
+
+// Ajout de la lettre dans le tableau des lettres déjà jouées
+
+// Vérification du pendu
+
+// Vérification du mot
+// Si mot trouvé, affichage du mot et message de victoire
